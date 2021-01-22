4458c68071dc0d6eb0f8ba378a9d1c02a0b1f198
CreamK <51256470+CreamK@users.noreply.github.com>
2021-01-22 09:53:34 +0800

    Update BBB.js

--- BBB.js
+++ BBB.js
@@ -30,8 +30,8 @@ hostname = bububao.duoshoutuan.com,
 
 const $ = new Env('步步寶')
 let notice = ''
-let CookieVal = $.getdata('bbb_ck')
-
+//let CookieVal = $.getdata('bbb_ck')
+let CookieVal = process.env.BBBCK
 if ($.isNode()) {
       console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
       console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}  =============\n`)
